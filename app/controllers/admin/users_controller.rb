0b15f04a056b040498c73f64d76240659f6005e7
alex-ismailov <alexx.ismailov@gmail.com>
2024-02-01 00:33:23 -0400

    Add user creation and updating functionality [les 6 ex 6]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,39 @@ class Admin::UsersController < Admin::ApplicationController
     @users = @q.result.order(:type).order(:id).page(params[:page]).per(params[:per_page])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to(admin_users_url)
+    else
+      render(:new_admin_user_url)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to(admin_users_url)
+    else
+      render(:edit)
+    end
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :type)
   end
 end
